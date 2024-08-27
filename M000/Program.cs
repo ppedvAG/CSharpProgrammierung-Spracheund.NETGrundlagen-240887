f707728c195065b2102d7b05dd0abc83b59bd4fd
ppedvLukasK <lukask@ppedv.de>
2024-08-27 16:10:45 +0200

    M000

--- M000/Program.cs
+++ M000/Program.cs
@@ -1,40 +1,45 @@
-﻿//Console.Write("Entfernung: ");
-//int meter = int.Parse(Console.ReadLine());
+﻿while (true)
+{
+	Console.Write("Gib eine Zahl ein: ");
+	int z1 = int.Parse(Console.ReadLine());
 
-//Console.Write("Stunde: ");
-//int std = int.Parse(Console.ReadLine());
+	Console.Write("Gib eine weitere Zahl ein: ");
+	int z2 = int.Parse(Console.ReadLine());
 
-//Console.Write("Minute: ");
-//int min = int.Parse(Console.ReadLine());
+	Console.WriteLine("Gib eine Rechenart ein: ");
+	foreach (Rechenarten r in Enum.GetValues<Rechenarten>())
+	{
+		Console.WriteLine($"{(int) r}: {r}");
+	}
+	Rechenarten op = Enum.Parse<Rechenarten>(Console.ReadLine());
 
-//Console.Write("Sekunde: ");
-//int sek = int.Parse(Console.ReadLine());
+	switch (op)
+	{
+		case Rechenarten.Add:
+			Console.WriteLine($"Ergebnis: {z1 + z2}");
+			break;
+		case Rechenarten.Sub:
+			Console.WriteLine($"Ergebnis: {z1 - z2}");
+			break;
+		case Rechenarten.Mult:
+			Console.WriteLine($"Ergebnis: {z1 * z2}");
+			break;
+		case Rechenarten.Div:
+			if (z2 == 0)
+				Console.WriteLine("Zahl 2 darf nicht 0 sein (Division durch 0 nicht möglich)");
+			else
+				Console.WriteLine($"Ergebnis: {(double) z1 / z2}");
+			break;
+	}
 
-//double gesamt = std * 3600 + min * 60 + sek;
+	Console.WriteLine("Enter zum Wiederholen");
+	if (Console.ReadKey(true).Key != ConsoleKey.Enter)
+		break;
+	Console.Clear();
+}
 
-//Console.WriteLine($"Meter/Sekunde: \t\t{Math.Round(meter/gesamt, 2)}");
-//Console.WriteLine($"Kilometer/Stunde: \t{Math.Round(meter / gesamt * 3.6, 2)}");
-//Console.WriteLine($"Meilen/Stunde: \t\t{Math.Round(meter / gesamt * 3.6 * 0.621, 2)}");
 
-//int[] zahlen = [16, 44, 38, 92, 77];
-//Console.Write("Gib einen Tipp ab: ");
-//int tipp = int.Parse(Console.ReadLine());
-//if (tipp >= 0 && tipp <= 100)
-//{
-//	if (zahlen.Contains(tipp))
-//		Console.WriteLine("Glückwunsch!");
-//	else
-//		Console.WriteLine("Leider kein Gewinn.");
-//}
-
-int jahr = 1800;
-bool schaltjahr = false;
-if (jahr % 4 == 0)
-	schaltjahr = true;
-if (jahr % 100 == 0)
-	schaltjahr = false;
-if (jahr % 400 == 0)
-	schaltjahr = true;
-Console.WriteLine(schaltjahr ? $"{jahr} ist ein Schaltjahr" : $"{jahr} ist kein Schaltjahr");
-
-DateTime.IsLeapYear(jahr);
+enum Rechenarten
+{
+	Add = 1, Sub, Mult, Div
+}
