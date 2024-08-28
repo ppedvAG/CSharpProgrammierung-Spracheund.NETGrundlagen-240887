0ce823d068498e4ffbddd5dba9570b64f69a61d2
ppedvLukasK <lukask@ppedv.de>
2024-08-28 15:17:56 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,18 @@
+﻿namespace M008;
+
+public class AccessModifier
+{
+	public string Name { get; set; } //public: Von überall angreifbar (innerhalb und außerhalb vom Projekt)
+
+	private int Alter { get; set; } //private: Nur innerhalb der Klasse angreifbar
+
+	internal string Adresse { get; set; } //internal: Von überall angreifbar, aber nur innerhalb des Projekts
+
+	//////////////////////////////////////////////
+	
+	protected int Gehalt { get; set; } //protected: Wie private, aber auch in Unterklassen sichtbar
+
+	protected private string Job { get; set; } //protected private: Wie protected, aber nur im Projekt sichtbar
+
+	protected internal string Haarfarbe { get; set; } //protected internal: Im gesamten Projekt sichtbar (Internal) und zusätzlich in Unterklassen außerhalb
+}
---
+++ M008/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace M008;
+
+/// <summary>
+/// Vererbung
+/// 
+/// Oberklassen geben Strukturen für Unterklassen vor, Unterklassen übernehmen diese Strukturen
+/// Felder/Propeties/Methoden werden vererbt (von der Oberklasse an die Unterklasse weitergegeben)
+/// 
+/// WICHTIG: Keine Mehrfachvererbung
+/// </summary>
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch(20, "Max");
+		m.Alter = 20; //Alter und Bewegen wurden nicht in Mensch definiert -> Vererbt
+		m.Bewegen(10);
+
+		Katze k = new Katze(3);
+		k.Alter = 3;
+		k.Bewegen(20);
+
+		m.Bewegen2(10); //Max bewegt sich um 10m
+		k.Bewegen2(10); //Lebewesen bewegt sich um 10m
+	}
+}
+
+/// <summary>
+/// Oberklasse Lebewesen, gibt Eigenschaften für die Unterklassen vor, die allgemein gültig sind
+/// 
+/// Eigenschaften: Alter, Körpergröße, Name, ...
+/// Funktionen: Bewegen, ...
+/// </summary>
+public class Lebewesen
+{
+	public int Alter { get; set; }
+
+	/// <summary>
+	/// Wenn ein Konstruktor in einer Oberklasse definiert wird, muss in den Unterklassen eine Verkettung stattfinden
+	/// </summary>
+	/// <param name="alter"></param>
+	public Lebewesen(int alter)
+	{
+		Alter = alter;
+	}
+
+	public void Bewegen(int distanz)
+	{
+		Console.WriteLine($"Lebewesen bewegt sich um {distanz}m");
+	}
+
+	/// <summary>
+	/// virtual
+	/// 
+	/// Funktion, welche überschrieben werden kann von den Unterklassen
+	/// Hier kann eine Basisimplementation existieren, in den Unterklassen kann diese überschrieben werden
+	/// </summary>
+	public virtual void Bewegen2(int distanz)
+	{
+		Console.WriteLine($"Lebewesen bewegt sich um {distanz}m");
+	}
+}
+
+/// <summary>
+/// Mit : [Klassenname] kann eine Vererbung hergestellt werden
+/// 
+/// Strg + .: Generate Constructor
+/// </summary>
+public class Mensch : Lebewesen
+{
+	/// <summary>
+	/// Neue Felder können im Konstruktor einfach hinzugefügt werden
+	/// </summary>
+	public string Name { get; set; }
+
+	public Mensch(int alter, string name) : base(alter) //Hier werden Verkettungen mit base(...) gemacht
+	{
+		Name = name;
+	}
+
+	/// <summary>
+	/// Override + Abstand -> Vorschläge
+	/// Akzeptieren mit Enter
+	/// </summary>
+	public override void Bewegen2(int distanz)
+	{
+		//base.: Methode aus der Oberklasse benutzen
+		//Vorallem nützlich bei Überschreibungen, gibt Zugriff auf die Basisimplementation
+		//base.Bewegen2(distanz);
+
+		Console.WriteLine($"{Name} bewegt sich um {distanz}m");
+	}
+}
+
+public class Katze : Lebewesen
+{
+	public Katze(int alter) : base(alter) { }
+}
